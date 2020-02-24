28065b7ba7ef418724894a53171517163407530f
Jacek Olszowski <jacek.olszowski1@gmail.com>
2020-02-24 21:29:35 +0100

    define arrays

---
+++ app.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+const genders = ['M', 'F'];
+
+console.log('genders', genders);
+
+const maleNames = ['Jacek', 'Wiktor', 'Karol', 'Błażej', 'Andrzej', 'Adam', 'Bartek', 'Kuba', 'Tomek', 'Piotr'];
+
+console.log(maleNames);
+
+
+const femaleNames = ['Agnieszka', 'Daria', 'Dagmara', 'Dorota', 'Marta', 'Anna', 'Ola', 'Elżbieta', 'Lena', 'Katarzyna'];
+
+const lastNames = ['Kiraga', 'Pluta', 'Sosna', 'Jaworek', 'Kwiat', 'Okoń'];
+
+
+console.log(femaleNames);
+
+console.log(lastNames);
