3f9ee82aa5a53aa81faafaf56b808b0c2700b444
Jacek Olszowski <jacek.olszowski1@gmail.com>
2020-02-25 05:23:08 +0100

    add randChoiceNumber func

--- app.js
+++ app.js
@@ -20,8 +20,17 @@ console.log(lastNames);
 
 // const people = []; 
 
+const ages = { min: 18, max: 78 };
+
 const randChoice = arr => {
     return arr[Math.floor(Math.random() * arr.length)];
 };
 
 console.log(randChoice(femaleNames));
+
+
+const randChoiceNumber = ({ min, max }) => {
+    return Math.floor(Math.random() * (max - min +1 )) + min;
+};
+
+console.log(randChoiceNumber(ages));
